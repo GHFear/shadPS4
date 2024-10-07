05b00914466fc06274953f0f31eec735fc40b2ec
GHFear <ghfear@hotmail.com>
2024-10-07 18:45:39 +0200

    updated for v0.3.1

--- autosetup.sh
+++ autosetup.sh
@@ -26,12 +26,11 @@ sudo apt-get update && sudo apt install libssl-dev libx11-dev libxext-dev libway
 # Install dependencies pack 2 : Required for Linux Mint 22
 sudo apt-get install build-essential libasound2-dev libpulse-dev libopenal-dev zlib1g-dev libedit-dev libvulkan-dev libudev-dev git libevdev-dev libsdl2-2.0 libsdl2-dev libjack-dev libsndio-dev
 
+# Install dependencies pack 3 : Required for Linux Mint 22
+sudo apt-get install qt6-multimedia-dev
+
 # Clone ShadPS4 repository recursively.
 git clone -b "$branchname" --recursive --single-branch https://github.com/shadps4-emu/shadPS4.git
-git clone -b filedpot --single-branch https://github.com/GHFear/shadPS4-mint22.git
-
-# Copy patched libavutil/error.h
-cp "shadPS4-mint22/externals/patched-libavutil-error/error.h" "shadPS4/externals/ffmpeg-core/include/libavutil/error.h"
 
 # Move into the shadPS4 directory
 cd shadPS4
